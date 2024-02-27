030b1ca89c78f2e643cd881a24e808ab504797aa
Aramdhann <radityamochamad@gmail.com>
2024-02-27 16:25:07 +0700

    deleted comment

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,8 +1,8 @@
 <link rel="shortcut icon" href="{{ url('assets/images/logo.png') }}" type="image/x-icon">
 <title>Warehouse</title>
+
 <!-- Scripts -->
 @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
-{{-- <link rel="stylesheet" href="assets/vendors/bootstrap/css/bootstrap.min.css"> --}}
 <link rel="stylesheet" href="{{ url('assets/vendors/boxicons/css/boxicons.min.css') }}">
 <link rel="stylesheet" href="{{ url('assets/css/style.css') }}">
